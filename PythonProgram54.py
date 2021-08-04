fc1fb4dd0ff3682024486f8cdd12fc943cc3e8dd
Tanzina Rahman Mona <77913975+TanzinaRahman@users.noreply.github.com>
2021-08-04 14:35:42 +0600

    Create PythonProgram54.py

---
+++ PythonProgram54.py
@@ -0,0 +1,12 @@
+#Exception Handling part 2
+
+def voter (age):
+    if age < 18:
+        raise ValueError("Invalid voter")
+    return "You are allowed to vote"
+try:
+    print(voter(19))
+    print(voter(17))
+except ValueError as e:
+    print(e)
+
